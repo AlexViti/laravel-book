f3b2427d8e418cc8477855dd114bfe8b805b4b0c
AlexViti <96179738+AlexViti@users.noreply.github.com>
2022-05-10 18:33:49 +0200

    Add titles

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,9 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{assets('title')}}</title>
+    <title> </title>
 </head>
 <body>
-    
+    @foreach ($books as $book)
+        <h1>{{ $book->title }}</h1>
+    @endforeach
 </body>
 </html>
